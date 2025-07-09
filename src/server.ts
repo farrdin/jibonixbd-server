106f260f2d0ded7e6812081f2aad182567141424
Rocky Haque <rockyhaque99@gmail.com>
2025-07-10 00:49:37 +0600

    feat: project setup initialized

--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,44 @@
 // server.ts
-import http from 'http';
-import pkg from 'pg';
-import { handleRequest } from './app';
+import http from 'http'
+import pkg from 'pg'
+import { handleRequest } from './app'
+import dotenv from 'dotenv'
 
-const { Pool } = pkg;
-const PORT = 5000;
+dotenv.config()
+
+const { Pool } = pkg
+const PORT = 5000
 
 // Initialize DB pool
 const pool = new Pool({
-  connectionString: 'postgresql://postgres:12345@localhost:5432/pure_node?schema=public',
-});
+  connectionString: process.env.DATABASE_URL
+})
 
 // Test DB connection
-pool.connect()
-  .then(client => {
-    console.log('Connected to PostgreSQL database successfully!');
-    client.release();
+pool
+  .connect()
+  .then((client) => {
+    console.log('Connected to PostgreSQL database successfully!')
+    client.release()
 
     // Start server only after DB is connected
-    const server = http.createServer((req, res) => handleRequest(req, res, pool));
+    const server = http.createServer((req, res) =>
+      handleRequest(req, res, pool)
+    )
 
     server.listen(PORT, () => {
-      console.log(`Jibonix server is running on ${PORT}`);
-    });
+      console.log(`Jibonix server is running on ${PORT}`)
+    })
 
     // Graceful shutdown
     process.on('SIGINT', async () => {
-      console.log('Closing PostgreSQL connection pool...');
-      await pool.end();
-      console.log('PostgreSQL connection pool closed.');
-      process.exit(0);
-    });
+      console.log('Closing PostgreSQL connection pool...')
+      await pool.end()
+      console.log('PostgreSQL connection pool closed.')
+      process.exit(0)
+    })
   })
-  .catch(err => {
-    console.error('Error connecting to PostgreSQL:', err.stack);
-    process.exit(1);
-  });
+  .catch((err) => {
+    console.error('Error connecting to PostgreSQL:', err.stack)
+    process.exit(1)
+  })
