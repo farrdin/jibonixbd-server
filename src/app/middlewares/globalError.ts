b30e4f021978ea9bda333f91ee91b84188018ca2
Fardin Ahmed Alif <fardinahmed.dev@gmail.com>
2025-08-07 02:25:09 +0600

    fix: Router name Fixed

--- src/app/middlewares/globalError.ts
+++ src/app/middlewares/globalError.ts
@@ -1,14 +1,26 @@
 import { ServerResponse } from 'http'
 import { sendJson } from '../utils'
+import AppError from '../errors/appError'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function globalError(res: ServerResponse, err: any, statusCode = 500) {
   console.error('🔥 Global Error:', err)
-  sendJson(res, statusCode, {
-    success: false,
-    message: 'Internal Server Error',
-    statusCode,
-    error: err,
-    stack: process.env.NODE_ENV === 'development' ? err?.stack : undefined
-  })
+
+  if (err instanceof AppError) {
+    sendJson(res, err.statusCode, {
+      success: false,
+      message: err.message,
+      statusCode: err.statusCode,
+      error: err,
+      stack: process.env.NODE_ENV === 'development' ? err.stack : undefined
+    })
+  } else {
+    sendJson(res, statusCode, {
+      success: false,
+      message: 'Internal Server Error',
+      statusCode,
+      error: err,
+      stack: process.env.NODE_ENV === 'development' ? err.stack : undefined
+    })
+  }
 }
