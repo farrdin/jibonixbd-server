2dea9021440f47804fe1dc74707b6576d7a0bc51
Fardin Ahmed Alif <fardinahmed.dev@gmail.com>
2025-08-11 02:51:00 +0600

    Fix: routes refactor

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,14 +1,9 @@
 import { IncomingMessage, ServerResponse } from 'http'
 import { Pool } from 'pg'
 import userRouter from '../modules/user/user.router'
-import volunteerRouter from '../modules/volunteer/volunteer.router'
 import authRouter from '../modules/auth/auth.router'
-import victimRouter from '../modules/victim/victim.router'
-import adminRouter from '../modules/admin/admin.router'
 import reliefRequestRouter from '../modules/reliefRequest/reliefRequest.router'
 import inventoryRouter from '../modules/inventory/inventory.router'
-import donorRouter from '../modules/donor/donor.router'
-import moderatorRouter from '../modules/moderator/moderator.router'
 import donationRouter from '../modules/donation/donation.router'
 import disasterRouter from '../modules/disaster/disaster.router'
 
@@ -21,11 +16,6 @@ type RouteHandler = (
 const moduleRoutes: { path: string; route: RouteHandler }[] = [
   { path: '/auth', route: authRouter },
   { path: '/users', route: userRouter },
-  { path: '/admin', route: adminRouter },
-  { path: '/moderator', route: moderatorRouter },
-  { path: '/volunteer', route: volunteerRouter },
-  { path: '/donor', route: donorRouter },
-  { path: '/victim', route: victimRouter },
   { path: '/disaster', route: disasterRouter },
   { path: '/inventory', route: inventoryRouter },
   { path: '/relief-request', route: reliefRequestRouter },
